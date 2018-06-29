1a2e57a1f1e3e7febf7c17e0ac9088078b8036e0
Tim Franssen <tim.franssen@nedap.com>
2018-06-29 15:57:17 +0200

    Fix header magic on mobile when the menu opens

--- assets/javascripts/navigation.js
+++ assets/javascripts/navigation.js
@@ -1,10 +1,20 @@
-window.addEventListener('scroll', function() {
-  var header = document.getElementById("site-header");
+function updateFixedness() {
+  var scrolledDown   = window.pageYOffset > 0;
+  var mobileMenuOpen = document.getElementById('nav-trigger').checked;
+  const header       = document.getElementById("site-header");
+
   if (!header) {return;}
 
-  if (window.pageYOffset > 0) {
+  if(scrolledDown || mobileMenuOpen) {
     header.classList.add("fixed");
   } else {
     header.classList.remove("fixed");
   }
+}
+
+window.addEventListener('scroll', updateFixedness);
+
+window.addEventListener('load', function() {
+  document.getElementById('nav-trigger')
+          .addEventListener('change', updateFixedness);
 });
