096cfb7317906a5db55866e7d039bdcadbaa07e9
Dillen Meijboom <work@dillen.dev>
2023-07-27 21:16:00 +0200

    feat: enable GFM (#184)

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,8 +1,10 @@
 import { remark } from "remark";
+import gfm from "remark-gfm";
 import html from "remark-html";
 
 export async function render(source: string): Promise<string> {
   const output = await remark()
+    .use(gfm)
     .use(html, { sanitize: false })
     .process(source);
   return output.toString();
