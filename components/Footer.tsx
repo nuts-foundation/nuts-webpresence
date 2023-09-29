befa973ec016a5e397ba99552502ee7a722365cb
Marinka Geijsen <135126266+Saldoo@users.noreply.github.com>
2023-09-29 15:21:57 +0200

    Update Footer.tsx
    Footer menu deel verwijzing naar Linkedin, Youtube en Podcast eruit halen.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,17 +17,7 @@ export default function Footer({ items }: Props) {
           </div>))}
         </div>))}
         <div className="text-center sm:text-left col-span-12 pt-5 md:pt-0 sm:col-span-6 md:col-span-3 space-y-5">
-          <div>
-            <a href="https://linkedin.com/company/stichting-nuts" className="text-white hover:underline">Nuts op LinkedIn</a>
-          </div>
-          <div>
-            <a href="https://www.youtube.com/channel/UCJtbrUe2TphkzDi2lPY5yYQ" className="text-white hover:underline">Nuts op YouTube</a>
-          </div>
-          <div>
-            <a href="https://nuts.nl/podcast" className="text-white hover:underline">Going Nuts podcast</a>
-          </div>
-        </div>
-      </div>
+        
     </div>
     <div className="bg-dark py-8">
       <div className="text-white container mx-auto flex justify-between items-center">
