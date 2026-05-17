90e184142e65f3f755c02cb820d6612a3a4baab5
Marinka Geijsen-Heijnis <135126266+Saldoo@users.noreply.github.com>
2026-05-17 17:18:42 +0200

    Create ToepassingenPage component with data fetching
    This file defines the ToepassingenPage component, which renders content using Markdown and includes the CategoryNuts component. It also implements getStaticProps to fetch data for the page.

---
+++ pages/toepassingen.tsx
@@ -0,0 +1,23 @@
+import { getContent } from "../lib/api";
+import Markdown from "../components/Markdown";
+import Layout from "../components/Layout";
+import CategoryNuts from "../components/CategoryNuts";
+
+export default function ToepassingenPage({ toepassingen }: any) {
+  return (
+    <Layout>
+      <div className="container mx-auto py-16 max-w-[960px]">
+        <Markdown html={toepassingen.content} className="prose max-w-none" />
+        <CategoryNuts />
+      </div>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      toepassingen: await getContent("toepassingen"),
+    },
+  };
+}
