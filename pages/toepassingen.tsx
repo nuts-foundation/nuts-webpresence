50367fd3aab3c4907d81e93e5f79629adc35e152
Marinka Geijsen-Heijnis <135126266+Saldoo@users.noreply.github.com>
2026-05-21 14:24:35 +0200

    Update container class for ToepassingenPage

--- pages/toepassingen.tsx
+++ pages/toepassingen.tsx
@@ -6,7 +6,7 @@ import CategoryNuts from "../components/CategoryNuts";
 export default function ToepassingenPage({ toepassingen }: any) {
   return (
     <Layout>
-      <div className="container mx-auto py-16 max-w-[960px]">
+      <div className="container mx-auto max-w-5xl px-4 py-16">
         <Markdown html={toepassingen.content} className="prose max-w-none" />
         <CategoryNuts />
       </div>
